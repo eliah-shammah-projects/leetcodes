05ce66d0907f9ed51bdafd933b42b2b664dc58f7
Eliahu Shammah <eliahunetanel@gmail.com>
2026-02-05 15:04:55 +0200

    another

---
+++ more_zeros.py
@@ -0,0 +1,18 @@
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+class Solution:
+    def moveZeroes(self, nums) -> None:
+        left = 0 
+        for i in range (len(nums)):
+            if nums[i] != 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+        
+            
+        
+            
+
+
+        
